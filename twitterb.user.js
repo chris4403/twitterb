1305705a4b475f4700fe0b718d085cce006251b1
chris <chris4403@hatena.ne.jp>
2009-10-22 18:32:46 +0900

    use unsafeWindow

--- twitterb.user.js
+++ twitterb.user.js
@@ -2,8 +2,10 @@
 // @name        twitterb 
 // @description auto fillin B!
 // @namespace   http://dev.chrisryu.com/
-// @include     http://twitter/*
-jQuery('#status').keyup(function(){
-    var v = this.value;
-    if(v.match(/http(|s):\/\//) && !v.match(/B!/)) this.value = ' B! ' + v;
-});
+// @include     http://twitter.com/*
+(function() {
+    unsafeWindow.jQuery('#status').keyup(function(){
+        var v = this.value;
+        if(v.match(/http(|s):\/\//) && !v.match(/B!/)) this.value = ' B! ' + v;
+    });
+})();
